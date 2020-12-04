23891042f1d702bc1b7d864cd5a5454c54624192
thanhtung <tung.nguyen@se-ec.com.vn>
2020-12-04 17:35:55 +0700

    04122020

--- src/templates/js/default.js
+++ src/templates/js/default.js
@@ -62,39 +62,115 @@ $(document).ready(function () {
 
 if ($('#index').length > 0) {
 	$('.owl-partner').owlCarousel({
-		rtl:true,
-		loop:true,
-		margin:10,
-		nav:false,
-		dots:false,
-		responsive:{
-				0:{
-						items:2
-				},
-				600:{
-						items:3
-				},
-				1000:{
-						items:5
-				}
+		rtl: true,
+		loop: true,
+		margin: 10,
+		nav: false,
+		dots: false,
+		responsive: {
+			0: {
+				items: 2
+			},
+			600: {
+				items: 3
+			},
+			1000: {
+				items: 5
+			}
 		}
 	})
 	$('.owl-news').owlCarousel({
-		rtl:true,
-		loop:true,
-		margin:25,
-		nav:false,
-		dots:false,
-		responsive:{
-				0:{
-						items:1
-				},
-				768:{
-						items:2
-				},
-				1000:{
-						items:3
-				}
+		rtl: true,
+		loop: true,
+		margin: 25,
+		nav: false,
+		dots: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			1000: {
+				items: 3
+			}
 		}
 	})
+}
+
+// stepForm
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+
+function showTab(n) {
+	// This function will display the specified tab of the form...
+	var x = document.getElementsByClassName("tab");
+	x[n].style.display = "block";
+	//... and fix the Previous/Next buttons:
+	if (n == 0) {
+		document.getElementById("prevBtn").style.display = "none";
+	} else {
+		document.getElementById("prevBtn").style.display = "inline";
+	}
+	if (n == (x.length - 1)) {
+		document.getElementById("nextBtn").innerHTML = "Hoàn Thành";
+	} else {
+		document.getElementById("nextBtn").innerHTML = "Tiếp theo";
+	}
+	//... and run a function that will display the correct step indicator:
+	fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+	// This function will figure out which tab to display
+	var x = document.getElementsByClassName("tab");
+	// Exit the function if any field in the current tab is invalid:
+	if (n == 1 && !validateForm()) return false;
+	// Hide the current tab:
+	x[currentTab].style.display = "none";
+	// Increase or decrease the current tab by 1:
+	currentTab = currentTab + n;
+	// if you have reached the end of the form...
+	if (currentTab >= x.length) {
+		// ... the form gets submitted:
+		document.getElementById("regForm").submit();
+		return false;
+	}
+	// Otherwise, display the correct tab:
+	showTab(currentTab);
+}
+
+function validateForm() {
+	// This function deals with validation of the form fields
+	var x, y, i, valid = true;
+	x = document.getElementsByClassName("tab");
+	y = x[currentTab].getElementsByTagName("input");
+	// A loop that checks every input field in the current tab:
+	for (i = 0; i < y.length; i++) {
+		// If a field is empty...
+		if (y[i].value == "") {
+			// add an "invalid" class to the field:
+			y[i].className += " invalid";
+			// and set the current valid status to false
+			valid = false;
+		}
+	}
+	// If the valid status is true, mark the step as finished and valid:
+	if (valid) {
+		document.getElementsByClassName("step")[currentTab].className += " finish";
+	}
+	return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+	// This function removes the "active" class of all steps...
+	var i, x = document.getElementsByClassName("step");
+	for (i = 0; i < x.length; i++) {
+		x[i].className = x[i].className.replace(" active", "");
+	}
+	//... and adds the "active" class on the current step:
+	x[n].className += " active";
 }
